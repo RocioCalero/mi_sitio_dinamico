c8af9d9ce1b748fb034145c7c32a915d34bdb4dc
RocioCalero <calero.lopez.rocio.de.los.reyes@iescamas.es>
2025-10-06 11:03:49 +0200

    Conflicto resuelto combinando títulos

--- elementos/header.php
+++ elementos/header.php
@@ -1,4 +1,4 @@
 <header class="text-center bg-white p-3 rounded shadow-sm">
-  <h1 class="text-primary">Bienvenido a Camas</h1>
+  <h1 class="text-primary">Bienvenido a Camas – Mi Sitio Dinámico PHP</h1>
   <p class="text-muted">Usando include() por primera vez</p>
 </header>
